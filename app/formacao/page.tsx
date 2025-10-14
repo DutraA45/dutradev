c994b7f63b11ecbb2aa6c923bdaa891a93d3a29f
Jefferson Dutra <jeffinho.angelo@gmail.com>
2025-10-14 20:48:30 -0300

    some-changes

--- app/formacao/page.tsx
+++ app/formacao/page.tsx
@@ -29,9 +29,9 @@ export default function Formacao() {
               <h2 className="text-2xl font-semibold text-green-500 mb-1">PUC Minas</h2>
               <p className="text-white text-xl font-semibold mb-1">Pós Graduação - Desenvolvimento Fullstack</p>
               <p className="text-white text-base font-light mb-2">jan de 2024 - set de 2025 · 2 anos</p>
-              <p className="text-white text-base font-normal">
+              {/* <p className="text-white text-base font-normal">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas laoreet placerat accumsan. Donec pellentesque ultrices ex, ut pellentesque arcu. Suspendisse ac mauris in est dapibus varius vel vitae odio. Vestibulum consequat eros cursus arcu sollicitudin, ac sollicitudin diam dapibus. Cras dignissim finibus vehicula.
-              </p>
+              </p> */}
             </div>
           </div>
 
@@ -54,9 +54,9 @@ export default function Formacao() {
               <h2 className="text-2xl font-semibold text-green-500 mb-1">UNISAL</h2>
               <p className="text-white text-xl font-semibold mb-1">Graduação - Engenharia da Computação</p>
               <p className="text-white text-base font-light mb-2">2018 - 2024 · 6 anos</p>
-              <p className="text-white text-base font-normal">
+              {/* <p className="text-white text-base font-normal">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas laoreet placerat accumsan. Donec pellentesque ultrices ex, ut pellentesque arcu. Suspendisse ac mauris in est dapibus varius vel vitae odio. Vestibulum consequat eros cursus arcu sollicitudin, ac sollicitudin diam dapibus. Cras dignissim finibus vehicula.
-              </p>
+              </p> */}
             </div>
           </div>
 
@@ -79,9 +79,9 @@ export default function Formacao() {
               <h2 className="text-2xl font-semibold text-green-500 mb-1">São Miguel</h2>
               <p className="text-white text-xl font-semibold mb-1">Técnico em Eletrônica</p>
               <p className="text-white text-base font-light mb-2">2015 - 2017 · 3 anos</p>
-              <p className="text-white text-base font-normal">
+              {/* <p className="text-white text-base font-normal">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas laoreet placerat accumsan. Donec pellentesque ultrices ex, ut pellentesque arcu. Suspendisse ac mauris in est dapibus varius vel vitae odio. Vestibulum consequat eros cursus arcu sollicitudin, ac sollicitudin diam dapibus. Cras dignissim finibus vehicula.
-              </p>
+              </p> */}
             </div>
           </div>
           
