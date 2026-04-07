7ea53acdd41c33a373ef0867f6f77b9af4687b3c
Jefferson <jeffinho.angelo@gmail.com>
2026-04-07 18:46:11 -0300

    feat: new background Particles color

--- app/components/BackgroundParticles.tsx
+++ app/components/BackgroundParticles.tsx
@@ -24,7 +24,7 @@ export default function BackgroundParticles() {
       fpsLimit: 120,
       particles: {
         number: {
-          value: 40,
+          value: 60,
           density: { enable: true },
         },
         color: {
